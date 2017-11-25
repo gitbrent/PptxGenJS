599f4a41c81713aef6fb1e9c762e8a6213da723e
GitBrent <brentely@Brents-Air.home>
2017-11-25 14:18:23 -0600

    Updated viewProps.xml to match PPT

--- dist/pptxgen.js
+++ dist/pptxgen.js
@@ -4255,17 +4255,17 @@ var PptxGenJS = function(){
 	function makeXmlViewProps() {
 		var strXml = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>'+CRLF
 					+ '<p:viewPr xmlns:a="http://schemas.openxmlformats.org/drawingml/2006/main" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships" xmlns:p="http://schemas.openxmlformats.org/presentationml/2006/main">'
-					+ '<p:normalViewPr><p:restoredLeft sz="15620"/><p:restoredTop sz="94660"/></p:normalViewPr>'
+					+ '<p:normalViewPr><p:restoredLeft sz="15610" /><p:restoredTop sz="94613" /></p:normalViewPr>'
 					+ '<p:slideViewPr>'
-					+ '  <p:cSldViewPr>'
-					+ '    <p:cViewPr varScale="1"><p:scale><a:sx n="64" d="100"/><a:sy n="64" d="100"/></p:scale><p:origin x="-1392" y="-96"/></p:cViewPr>'
-					+ '    <p:guideLst><p:guide orient="horz" pos="2160"/><p:guide pos="2880"/></p:guideLst>'
+					+ '  <p:cSldViewPr snapToGrid="0" snapToObjects="1">'
+					+ '    <p:cViewPr varScale="1"><p:scale><a:sx n="119" d="100" /><a:sy n="119" d="100" /></p:scale><p:origin x="312" y="184" /></p:cViewPr>'
+					+ '    <p:guideLst />'
 					+ '  </p:cSldViewPr>'
 					+ '</p:slideViewPr>'
 					+ '<p:notesTextViewPr>'
-					+ '  <p:cViewPr><p:scale><a:sx n="100" d="100"/><a:sy n="100" d="100"/></p:scale><p:origin x="0" y="0"/></p:cViewPr>'
+					+ '  <p:cViewPr><p:scale><a:sx n="1" d="1" /><a:sy n="1" d="1" /></p:scale><p:origin x="0" y="0" /></p:cViewPr>'
 					+ '</p:notesTextViewPr>'
-					+ '<p:gridSpacing cx="78028800" cy="78028800"/>'
+					+ '<p:gridSpacing cx="76200" cy="76200" />'
 					+ '</p:viewPr>';
 		return strXml;
 	}
