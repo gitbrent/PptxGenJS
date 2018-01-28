ec9254c4e5eecb9dc35d3ad00cd236483f97f95c
GitBrent <brentely@Brents-Air.home>
2018-01-28 15:00:24 -0600

    First build

---
+++ website/siteConfig.js
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+/* List of projects/orgs using your project for the users page */
+const users = [
+	{
+		caption: 'PptxGenJS',
+		image: '/PptxGenJS/img/pptxgenjs.svg',
+		infoLink: 'https://gitbrent.github.io/PptxGenJS/',
+		pinned: true,
+	},
+];
+
+const siteConfig = {
+	title: 'PptxGenJS Home' /* title for your website */,
+	tagline: 'A website for testing',
+	url: 'https://gitbrent.github.io' /* your website url */,
+	baseUrl: '/PptxGenJS/' /* base url for your project */,
+	projectName: 'PptxGenJS',
+	headerLinks: [
+		{doc: 'doc1', label: 'Docs'},
+		{doc: 'doc4', label: 'API'},
+		{page: 'help', label: 'Help'},
+		{blog: true, label: 'Blog'},
+	],
+	users,
+	/* path to images for header/footer */
+	headerIcon: 'img/pptxgenjs.svg',
+	footerIcon: 'img/pptxgenjs.svg',
+	favicon: 'img/favicon.png',
+	/* colors for website */
+	colors: {
+		primaryColor: '#2E8555',
+		secondaryColor: '#205C3B',
+	},
+	// This copyright info is used in /core/Footer.js and blog rss/atom feeds.
+	copyright:
+		'Copyright © ' +
+		new Date().getFullYear() +
+		' Brent Ely',
+	// organizationName: 'deltice', // or set an env variable ORGANIZATION_NAME
+	projectName: 'PptxGenJS', // or set an env variable PROJECT_NAME
+	highlight: {
+		// Highlight.js theme to use for syntax highlighting in code blocks
+		theme: 'default',
+	},
+	scripts: ['https://buttons.github.io/buttons.js'],
+	// You may provide arbitrary config keys to be used as needed by your template.
+	repoUrl: 'https://github.com/gitbrent/PptxGenJS',
+};
+
+module.exports = siteConfig;
