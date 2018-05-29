40ae58095d71bdfc205d015911d23e48b588423f
GitBrent <brentely@Brents-Air.home>
2018-05-29 00:07:07 -0500

    fixed/updated footer links

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -56,16 +56,16 @@ class Footer extends React.Component {
 					<div>
 						<h5>Docs</h5>
 						<a
-							href={this.docUrl('installation.html', this.props.language)}
-							onClick={()=>ga('send','event','Link','click','Link-installation')}>
+							href={this.docUrl('quick-start.html', this.props.language)}
+							onClick={()=>ga('send','event','Link','click','Link-quickstart')}>
 							Getting Started With PptxGenJS
 						</a>
 						<a
-							href={this.docUrl('usage-basic-create.html', this.props.language)}
+							href={this.docUrl('installation.html', this.props.language)}
 							onClick={this.handleClick}>
 							PowerPoint Library API Reference
 						</a>
-						<a href={this.docUrl('usage-basic-create.html', this.props.language)}>
+						<a href={this.docUrl('usage-pres-create.html', this.props.language)}>
 							PowerPoint JavaScript Code Samples
 						</a>
 					</div>
