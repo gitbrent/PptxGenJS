5a136b50d7eac1a57ceddd4a49447ab37e06f20a
GitBrent <gitbrent@users.noreply.github.com>
2018-07-07 17:47:53 -0500

    Added new `hidden` property to `slide` (Issue #367)
    Co-Authored-By: Reima Frgos <reimafrgos@users.noreply.github.com>

--- dist/pptxgen.js
+++ dist/pptxgen.js
@@ -4059,7 +4059,7 @@ var PptxGenJS = function(){
 	function makeXmlSlide(objSlide) {
 		// STEP 1: Generate slide XML - wrap generated text in full XML envelope
 		var strXml = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>'+CRLF;
-		strXml += '<p:sld xmlns:a="http://schemas.openxmlformats.org/drawingml/2006/main" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships" xmlns:p="http://schemas.openxmlformats.org/presentationml/2006/main">';
+		strXml += '<p:sld xmlns:a="http://schemas.openxmlformats.org/drawingml/2006/main" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships" xmlns:p="http://schemas.openxmlformats.org/presentationml/2006/main"'+ (objSlide.slide.hidden ? ' show="0"' : '') +'>';
 		strXml += gObjPptxGenerators.slideObjectToXml(objSlide);
 		strXml += '<p:clrMapOvr><a:masterClrMapping/></p:clrMapOvr>';
 		strXml += '</p:sld>';
