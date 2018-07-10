6d840745d927c12afb7d846e53bdfd08d74295d6
Connor Bowman <conbow@users.noreply.github.com>
2018-07-10 08:25:58 -0400

    Fixed issue with placeholder p:ph tag being added to non-placeholder objects

--- dist/pptxgen.js
+++ dist/pptxgen.js
@@ -705,7 +705,7 @@ var PptxGenJS = function(){
 				var x = 0, y = 0, cx = getSmartParseNumber('75%','X'), cy = 0, placeholderObj;
 				var locationAttr = "", shapeType = null;
 
-				if (slideObject.layoutObj && slideObject.layoutObj.data) {
+				if (slideObject.layoutObj && slideObject.layoutObj.data && slideItemObj.options && slideItemObj.options.placeholder) {
 					placeholderObj = slideObject.layoutObj.data.filter(function(layoutObj){ return layoutObj.options.idx === slideItemObj.options.placeholder})[0];
 				}
 
