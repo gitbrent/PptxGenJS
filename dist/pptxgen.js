3a1585b09c4d54df81b4ecb9afdea5788ccb6c82
Michael Ayvazyan <m@ayvazyan.net>
2018-12-19 17:54:50 +0300

    minor cleanup

--- dist/pptxgen.js
+++ dist/pptxgen.js
@@ -3933,7 +3933,7 @@ var PptxGenJS = function(){
 				if ( textObj.options.bullet.type ) {
 					if ( textObj.options.bullet.type.toString().toLowerCase() == "number" ) {
 						paragraphPropXml += ' marL="'+ (textObj.options.indentLevel && textObj.options.indentLevel > 0 ? bulletLvl0Margin+(bulletLvl0Margin*textObj.options.indentLevel) : bulletLvl0Margin) +'" indent="-'+bulletLvl0Margin+'"';
-						const style = textObj.options.bullet.style ? textObj.options.bullet.style : 'arabicPeriod';
+						const style = textObj.options.bullet.style || 'arabicPeriod';
 						strXmlBullet = '<a:buSzPct val="100000"/><a:buFont typeface="+mj-lt"/><a:buAutoNum type="' + style + '"/>';
 					}
 				}
