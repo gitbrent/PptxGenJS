b28139c2addeb301a022f1037aba68abe62c802d
GitBrent <gitbrent@users.noreply.github.com>
2019-01-02 22:30:49 -0600

    updated gulp to 4.0; updated build system

---
+++ website/README.txt
@@ -0,0 +1,8 @@
+// build site
+npm run-script build
+
+// serve site locally
+npm run-script start
+
+// deploy site
+npm run-script deploy
--- website/gulpfile.js
+++ website/gulpfile.js
@@ -1,51 +1,38 @@
-/*
- * DESC: Combine/Minify CSS and take existing min JavaScript - replace head tags with content
- * WHY.: Google PageSpeed Insights scores of 99(mobile)/94(desktop) thats why!!
- */
+/*\
+|*| DESC: Combine/Minify CSS and take existing min JavaScript - replace head tags with content
+|*| WHY.: Google PageSpeed Insights [Lighthouse] scores of 100(mobile)/100(desktop) thats why!!
+\*/
+"use strict";
+
 var fs       = require('fs'),
-    gulp     = require('gulp'),
-    concat   = require('gulp-concat'),
-    replace  = require('gulp-string-replace'),
+	gulp     = require('gulp'),
+	concat   = require('gulp-concat'),
+	replace  = require('gulp-string-replace'),
 	cleanCSS = require('gulp-clean-css');
 
 var cssSrch1 = '<link rel="stylesheet" href="/PptxGenJS/css/main.css"/>';
-var cssSrch2 = '<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/hybrid.min.css"/>';
-var jvsSrch1 = /\<script type="text\/javascript" src="https:\/\/cdnjs.cloudflare.com\/ajax\/libs\/highlight.*.min.js"\>\<\/script\>/;
+var cssSrch2 = '<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.13.1/styles/hybrid.min.css"/>';
+var jvsSrch1 = /\<script src="https:\/\/cdnjs.cloudflare.com\/ajax\/libs\/highlight.*.min.js"\>\<\/script\>/;
 var jvsSrch2 = 'pptxgen.bundle.js">';
-
-/* ========== */
-var arrDeployTasks = ['deploy-css','deploy-html','deploy-index','deploy-img','deploy-help','deploy-sitemap'];
 
 gulp.task('deploy-css', ()=>{
 	return gulp.src('./build/PptxGenJS/css/main.css').pipe(gulp.dest('../css/'));
 });
-
 gulp.task('deploy-html', ()=>{
 	return gulp.src('./build/PptxGenJS/docs/*.html').pipe(gulp.dest('../docs/'));
 });
-
 gulp.task('deploy-index', ()=>{
 	return gulp.src('../index.perf.html', {base:'./'}).pipe(gulp.dest('../index.html'));
 });
-
 gulp.task('deploy-img', ()=>{
 	return gulp.src('./build/PptxGenJS/img/*.*').pipe(gulp.dest('../img/'));
 });
-
 gulp.task('deploy-help', ()=>{
 	return gulp.src('./build/PptxGenJS/help.html').pipe(gulp.dest('../'));
 });
-
 gulp.task('deploy-sitemap', ()=>{
 	return gulp.src('./build/PptxGenJS/sitemap.xml').pipe(gulp.dest('../'));
 });
-
-gulp.task('deploy', arrDeployTasks, ()=>{
-	console.log('Deploy tasks run:'+ arrDeployTasks.length );
-	console.log('DONE!');
-});
-
-/* ========== */
 
 gulp.task('min-css', ()=>{
 	// STEP 1: Inline both css files
@@ -56,14 +43,14 @@ gulp.task('min-css', ()=>{
 		.pipe(gulp.src('../css/style.bundle.css'));
 });
 
-gulp.task('default', ['min-css'], ()=>{
-	// STEP 2: Grab newly combined styles
+gulp.task('default', gulp.series('min-css', gulp.parallel(gulp.parallel('deploy-css','deploy-help','deploy-html','deploy-img','deploy-index','deploy-sitemap'))), ()=>{
+	// A: Grab newly combined styles
 	var strMinCss = fs.readFileSync('../css/style.bundle.css', 'utf8');
 	var strMinJvs = fs.readFileSync('../js/highlight.min.js', 'utf8');
 	//console.log('>> `style.bundle.css` lines = '+ strMinCss.split('\n').length);
 	//console.log('>> `highlight.min.js` lines = '+ strMinJvs.split('\n').length);
 
-	// STEP 3: Replace head tags with inline style/javascript
+	// B: Replace head tags with inline style/javascript
 	gulp.src('build/PptxGenJS/index.html')
 	.pipe(replace(cssSrch1, '', {logs:{ enabled:true }}))
 	.pipe(replace(cssSrch2, '\n<style>'+ strMinCss +'</style>\n', {logs:{ enabled:false }}))
