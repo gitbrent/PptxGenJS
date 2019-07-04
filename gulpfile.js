26b56fafd12e69aeae8e3ab9f27cf35a2fe0a254
Brent Ely <gitbrent@gmail.com>
2019-07-04 16:43:54 -0500

    updated build process for rollup

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,60 @@
-var gulp       = require('gulp'),
-	concat     = require('gulp-concat'),
-	sourcemaps = require('gulp-sourcemaps'),
-	ignore     = require('gulp-ignore'),
-	insert     = require('gulp-insert'),
-	uglify     = require('gulp-uglify'),
-	fs         = require('fs');
+const gulp = require("gulp"),
+  concat = require("gulp-concat"),
+  sourcemaps = require("gulp-sourcemaps"),
+  ignore = require("gulp-ignore"),
+  insert = require("gulp-insert"),
+  deleteLines = require("gulp-delete-lines"),
+  uglify = require("gulp-uglify");
+const rollup = require("rollup");
+const rollupTypescript = require("rollup-plugin-typescript2");
+const pkg = require("./package.json");
 
-var APP_VER = "", APP_BLD = "";
-gulp.task('get-vars', ()=>{
-	fs.readFileSync("dist/pptxgen.js", "utf8").split('\n')
-	.forEach((line)=>{
-		if ( line.indexOf('var APP_VER') > -1 ) APP_VER = line.split('=')[1].trim().replace(/\"+|\;+/gi,'');
-		if ( line.indexOf('var APP_BLD') > -1 ) APP_BLD = line.split('=')[1].trim().replace(/\"+|\;+/gi,'');
-	});
-	return gulp.src('README.md');
+gulp.task("build", () => {
+  return rollup
+    .rollup({
+      input: "./src/pptxgen.ts",
+      external: [
+        ...Object.keys(pkg.dependencies || {}),
+        ...Object.keys(pkg.peerDependencies || {})
+      ],
+      plugins: [rollupTypescript()]
+    })
+    .then(bundle => {
+      return bundle.write({
+        file: "./dist/pptxgen.es.js",
+        format: "es",
+        name: "pptxgen",
+        sourcemap: true
+      });
+    });
 });
-gulp.task('deploy-bundle', ()=>{
-	return gulp.src(['libs/*', 'dist/pptxgen.js'])
-		.pipe(concat('pptxgen.bundle.js'))
-		.pipe(uglify())
-		.pipe(insert.prepend('/* PptxGenJS '+APP_VER+'-'+APP_BLD+' */\n'))
-		.pipe(sourcemaps.init())
-		.pipe(ignore.exclude(["**/*.map"]))
-		.pipe(sourcemaps.write('./'))
-		.pipe(gulp.dest('dist/'));
-});
-gulp.task('deploy-min', ()=>{
-	return gulp.src(['dist/pptxgen.js'])
-		.pipe(concat('pptxgen.min.js'))
-		.pipe(uglify())
-		.pipe(insert.prepend('/* PptxGenJS '+APP_VER+'-'+APP_BLD+' */\n'))
-		.pipe(gulp.dest('dist/'));
+
+gulp.task("clean", () => {
+  return gulp
+    .src(["dist/pptxgen.es.js"])
+    .pipe(
+      deleteLines({
+        filters: [/^import |^export /i]
+      })
+    )
+    .pipe(concat("pptxgen.min.js"))
+    .pipe(uglify())
+    .pipe(
+      insert.prepend(
+        "/* PptxGenJS " +
+          pkg.version +
+          " @ " +
+          new Date().toISOString() +
+          " */\n"
+      )
+    )
+    .pipe(sourcemaps.init())
+    .pipe(ignore.exclude(["**/*.map"]))
+    .pipe(sourcemaps.write("./"))
+    .pipe(gulp.dest("dist/"));
 });
 
 // Build/Deploy
-gulp.task('default', gulp.series('get-vars', gulp.parallel('deploy-bundle','deploy-min')), ()=>{
-	console.log('Done');
+gulp.task("default", gulp.series("build", "clean"), () => {
+  console.log("... dist/pptxgen.min.js done!");
 });
