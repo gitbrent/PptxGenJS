eb76405488c1a78c4a9d0d469d9a464b7f512545
Brent Ely <gitbrent@gmail.com>
2019-07-06 00:10:54 -0500

    added quotes

--- src/enums.ts
+++ src/enums.ts
@@ -76,16 +76,16 @@ export const DEF_CELL_MARGIN_PT: Array<number> = [3, 3, 3, 3] // TRBL-style
 
 // ENUM
 export enum SCHEME_COLOR_NAMES {
-	TEXT1 = 'tx1',
-	TEXT2 = 'tx2',
-	BACKGROUND1 = 'bg1',
-	BACKGROUND2 = 'bg2',
-	ACCENT1 = 'accent1',
-	ACCENT2 = 'accent2',
-	ACCENT3 = 'accent3',
-	ACCENT4 = 'accent4',
-	ACCENT5 = 'accent5',
-	ACCENT6 = 'accent6',
+	'TEXT1' = 'tx1',
+	'TEXT2' = 'tx2',
+	'BACKGROUND1' = 'bg1',
+	'BACKGROUND2' = 'bg2',
+	'ACCENT1' = 'accent1',
+	'ACCENT2' = 'accent2',
+	'ACCENT3' = 'accent3',
+	'ACCENT4' = 'accent4',
+	'ACCENT5' = 'accent5',
+	'ACCENT6' = 'accent6',
 }
 
 export enum MASTER_OBJECTS {
