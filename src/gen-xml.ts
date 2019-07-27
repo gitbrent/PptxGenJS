4c960af7e29c8848e588c49737fd11bd642c543b
Brent Ely <gitbrent@gmail.com>
2019-07-26 23:32:18 -0500

    doc updates

--- src/gen-xml.ts
+++ src/gen-xml.ts
@@ -1031,33 +1031,36 @@ function genXmlTextRunProperties(opts, isDefault) {
 }
 
 /**
-* DESC: Builds <a:r></a:r> text runs for <a:p> paragraphs in textBody
-* EX:
-<a:r>
-  <a:rPr lang="en-US" sz="2800" dirty="0" smtClean="0">
-	<a:solidFill>
-	  <a:srgbClr val="00FF00">
-	  </a:srgbClr>
-	</a:solidFill>
-	<a:latin typeface="Courier New" pitchFamily="34" charset="0"/>
-  </a:rPr>
-  <a:t>Misc font/color, size = 28</a:t>
-</a:r>
+* Builds <a:r></a:r> text runs for <a:p> paragraphs in textBody
+* @param {Object} opts - various options
+* @param {string} paraText - various options
+* @return {string} XML string
+* @example
+* <a:r>
+*   <a:rPr lang="en-US" sz="2800" dirty="0" smtClean="0">
+* 	<a:solidFill>
+* 	  <a:srgbClr val="00FF00">
+* 	  </a:srgbClr>
+* 	</a:solidFill>
+* 	<a:latin typeface="Courier New" pitchFamily="34" charset="0"/>
+*   </a:rPr>
+*   <a:t>Misc font/color, size = 28</a:t>
+* </a:r>
 */
-function genXmlTextRun(opts, inStrText) {
-	var xmlTextRun = ''
-	var paraProp = ''
-	var parsedText
+function genXmlTextRun(opts, paraText:string):string {
+	let xmlTextRun = ''
+	let paraProp = ''
+	let arrLines = []
 
-	// ADD runProperties
-	var startInfo = genXmlTextRunProperties(opts, false)
+	// 1: ADD runProperties
+	let startInfo = genXmlTextRunProperties(opts, false)
 
-	// LINE-BREAKS/MULTI-LINE: Split text into multi-p:
-	parsedText = inStrText.split(CRLF)
-	if (parsedText.length > 1) {
+	// 2: LINE-BREAKS/MULTI-LINE: Split text into multi-p:
+	arrLines = paraText.split(CRLF)
+	if (arrLines.length > 1) {
 		var outTextData = ''
-		for (var i = 0, total_size_i = parsedText.length; i < total_size_i; i++) {
-			outTextData += '<a:r>' + startInfo + '<a:t>' + encodeXmlEntities(parsedText[i])
+		for (var i = 0, total_size_i = arrLines.length; i < total_size_i; i++) {
+			outTextData += '<a:r>' + startInfo + '<a:t>' + encodeXmlEntities(arrLines[i])
 			// Stop/Start <p>aragraph as long as there is more lines ahead (otherwise its closed at the end of this function)
 			if (i + 1 < total_size_i) outTextData += (opts.breakLine ? CRLF : '') + '</a:t></a:r>'
 		}
@@ -1065,7 +1068,7 @@ function genXmlTextRun(opts, inStrText) {
 	} else {
 		// Handle cases where addText `text` was an array of objects - if a text object doesnt contain a '\n' it still need alignment!
 		// The first pPr-align is done in makeXml - use line countr to ensure we only add subsequently as needed
-		xmlTextRun = (opts.align && opts.lineIdx > 0 ? paraProp : '') + '<a:r>' + startInfo + '<a:t>' + encodeXmlEntities(inStrText)
+		xmlTextRun = (opts.align && opts.lineIdx > 0 ? paraProp : '') + '<a:r>' + startInfo + '<a:t>' + encodeXmlEntities(paraText)
 	}
 
 	// Return paragraph with text run
@@ -1074,7 +1077,7 @@ function genXmlTextRun(opts, inStrText) {
 
 /**
  * Builds `<a:bodyPr></a:bodyPr>` tag
- * @param {Object} `objOptions` various options
+ * @param {Object} objOptions - various options
  * @return {string} XML string
  */
 function genXmlBodyProperties(objOptions): string {
