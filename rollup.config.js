902b445e5ff18f63f052d713f171987e54eb2e78
Brent Ely <gitbrent@gmail.com>
2019-07-31 12:33:52 -0500

    changed outdir dir to src/bld

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,21 @@
 import typescript from 'rollup-plugin-typescript2'
 import pkg from './package.json'
 export default {
-  input: 'src/pptxgen.ts',
-  output: [
-    {
-      file: pkg.main,
-      format: 'cjs',
-    },
-    {
-      file: pkg.module,
-      format: 'es',
-    },
-  ],
-  external: [
-    ...Object.keys(pkg.dependencies || {}),
-    ...Object.keys(pkg.peerDependencies || {}),
-  ],
-plugins: [
-    typescript({
-      typescript: require('typescript'),
-    }),
-  ],
+	input: 'src/pptxgen.ts',
+	output: [
+		{
+			file: './src/bld/' + pkg.main,
+			format: 'cjs'
+		},
+		{
+			file: './src/bld/' + pkg.module,
+			format: 'es'
+		}
+	],
+	external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
+	plugins: [
+		typescript({
+			typescript: require('typescript')
+		})
+	]
 }
