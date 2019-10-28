3d710a0b4334b04092f680133070aa6627356e85
Brent Ely <gitbrent@gmail.com>
2019-10-27 23:50:43 -0500

    Fixed cjs and es builds; formatted

--- gulpfile.js
+++ gulpfile.js
@@ -1,95 +1,82 @@
-const pkg = require("./package.json");
-const rollup = require("rollup");
-const rollupTypescript = require("rollup-plugin-typescript2");
-const { watch, series } = require("gulp");
-const gulp = require("gulp"),
-  concat = require("gulp-concat"),
-  sourcemaps = require("gulp-sourcemaps"),
-  ignore = require("gulp-ignore"),
-  insert = require("gulp-insert"),
-  deleteLines = require("gulp-delete-lines"),
-  uglify = require("gulp-uglify");
+const pkg = require('./package.json')
+const rollup = require('rollup')
+const rollupTypescript = require('rollup-plugin-typescript2')
+const { watch, series } = require('gulp')
+const gulp = require('gulp'),
+	concat = require('gulp-concat'),
+	sourcemaps = require('gulp-sourcemaps'),
+	ignore = require('gulp-ignore'),
+	insert = require('gulp-insert'),
+	deleteLines = require('gulp-delete-lines'),
+	uglify = require('gulp-uglify')
 
-gulp.task("build", () => {
-  return rollup
-    .rollup({
-      input: "./src/pptxgen.ts",
-      external: [
-        ...Object.keys(pkg.dependencies || {}),
-        ...Object.keys(pkg.peerDependencies || {})
-      ],
-      plugins: [rollupTypescript()]
-    })
-    .then(bundle => {
-      return bundle.write({
-        file: "./src/bld/pptxgen.gulp.js",
-        format: "iife",
-        name: "PptxGenJS",
-        globals: {
-            jszip: 'JSZip'
-        },
-        sourcemap: true,
-		definitions: true
-      });
-    });
-});
+gulp.task('build', () => {
+	return rollup
+		.rollup({
+			input: './src/pptxgen.ts',
+			external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
+			plugins: [rollupTypescript()]
+		})
+		.then(bundle => {
+			bundle.write({
+				file: './src/bld/pptxgen.gulp.js',
+				format: 'iife',
+				name: 'PptxGenJS',
+				globals: {
+					jszip: 'JSZip'
+				},
+				sourcemap: true,
+				definitions: true
+			})
+			return bundle
+		})
+		.then(bundle => {
+			bundle.write({
+				file: './src/bld/pptxgen.cjs.js',
+				format: 'cjs'
+			})
+			return bundle
+		})
+		.then(bundle => {
+			return bundle.write({
+				file: './src/bld/pptxgen.es.js',
+				format: 'es'
+			})
+		})
+})
 
-gulp.task("clean", () => {
-  return gulp
-    .src(["./src/bld/pptxgen.gulp.js"])
-    .pipe(concat("pptxgen.min.js"))
-    .pipe(uglify())
-    .pipe(
-      insert.prepend(
-        "/* PptxGenJS " +
-          pkg.version +
-          " @ " +
-          new Date().toISOString() +
-          " */\n"
-      )
-    )
-    .pipe(sourcemaps.init())
-    .pipe(ignore.exclude(["**/*.map"]))
-    .pipe(sourcemaps.write("./"))
-    .pipe(gulp.dest("./dist/"));
-});
+gulp.task('min', () => {
+	return gulp
+		.src(['./src/bld/pptxgen.gulp.js'])
+		.pipe(concat('pptxgen.min.js'))
+		.pipe(uglify())
+		.pipe(insert.prepend('/* PptxGenJS ' + pkg.version + ' @ ' + new Date().toISOString() + ' */\n'))
+		.pipe(sourcemaps.init())
+		.pipe(ignore.exclude(['**/*.map']))
+		.pipe(sourcemaps.write('./'))
+		.pipe(gulp.dest('./dist/'))
+})
 
-gulp.task("cjs", () => {
-  return gulp
-    .src(["./src/bld/pptxgen.cjs.js"])
-    .pipe(
-      insert.prepend(
-        "/* PptxGenJS " +
-          pkg.version +
-          " @ " +
-          new Date().toISOString() +
-          " */\n"
-      )
-    )
-    .pipe(gulp.dest("./dist/"));
-});
+gulp.task('cjs', () => {
+	return gulp
+		.src(['./src/bld/pptxgen.cjs.js'])
+		.pipe(insert.prepend('/* PptxGenJS ' + pkg.version + ' @ ' + new Date().toISOString() + ' */\n'))
+		.pipe(gulp.dest('./dist/'))
+})
 
-gulp.task("es", () => {
-  return gulp
-    .src(["./src/bld/pptxgen.es.js"])
-    .pipe(
-      insert.prepend(
-        "/* PptxGenJS " +
-          pkg.version +
-          " @ " +
-          new Date().toISOString() +
-          " */\n"
-      )
-    )
-    .pipe(gulp.dest("./dist/"));
-});
+gulp.task('es', () => {
+	return gulp
+		.src(['./src/bld/pptxgen.es.js'])
+		.pipe(insert.prepend('/* PptxGenJS ' + pkg.version + ' @ ' + new Date().toISOString() + ' */\n'))
+		.pipe(gulp.dest('./dist/'))
+})
 
 // Build/Deploy
-gulp.task("default", gulp.series("build", "clean", "cjs", "es"), () => {
-  console.log("... dist/pptxgen.min.js done!");
-});
+gulp.task('default', gulp.series('build', 'min', 'cjs', 'es'), () => {
+	console.log('... dist/pptxgen.min.js done!')
+})
 
 // Watch
 exports.default = function() {
-  watch("src/*.ts", series("build", "clean", "cjs", "es"));
-};
+	watch('src/*.ts', series('build', 'min', 'cjs', 'es'))
+}
