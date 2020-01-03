63d1204f473aed236bd37bd56ceb0ba117b0e93f
Brent Ely <gitbrent@gmail.com>
2020-01-02 23:53:03 -0600

    updated for v3.0.0

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -19,7 +19,7 @@ const tryCodeBlock = `
 	    "BONJOUR - CIAO - GUTEN TAG - HELLO - HOLA - NAMASTE - OLÀ - ZDRAS-TVUY-TE - こんにちは - 你好",
 	    { x:0, y:1, w:'100%', h:2, align:'c', color:'0088CC', fill:'F1F1F1', fontSize:24 }
 	);
-	pptx.save('PptxGenJS-Demo');
+	pptx.writeFile('PptxGenJS-Demo');
 `;
 
 /* ========== */
