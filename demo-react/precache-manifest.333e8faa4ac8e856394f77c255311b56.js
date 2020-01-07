e8d6dc0a7c6cddfcfd7026afbdcc56b47b008215
Brent Ely <gitbrent@gmail.com>
2020-01-06 23:38:53 -0600

    adding react-demp-app

---
+++ demo-react/precache-manifest.333e8faa4ac8e856394f77c255311b56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60df16bfa32e82b65e01c154dc52da11",
+    "url": "/PptxGenJS/react-demo/index.html"
+  },
+  {
+    "revision": "ae90e3f3de411f8766f5",
+    "url": "/PptxGenJS/react-demo/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "d98019040db7f154c341",
+    "url": "/PptxGenJS/react-demo/static/js/2.61dbd13b.chunk.js"
+  },
+  {
+    "revision": "ae90e3f3de411f8766f5",
+    "url": "/PptxGenJS/react-demo/static/js/main.7cead5c5.chunk.js"
+  },
+  {
+    "revision": "dc21765f7cf1a228528b",
+    "url": "/PptxGenJS/react-demo/static/js/runtime-main.92350afb.js"
+  },
+  {
+    "revision": "bcad93aedc1415f5c6f562983a74d71d",
+    "url": "/PptxGenJS/react-demo/static/media/logo.bcad93ae.svg"
+  }
+]);
