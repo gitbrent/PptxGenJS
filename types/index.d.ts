5d0b7348dbc922454a4805a7b1de2eea73ad5965
Stephen Cronin <stephen.cronin@reaktor.com>
2020-01-09 13:30:13 -0500

    Add missing margin type from ITextOpts

--- types/index.d.ts
+++ types/index.d.ts
@@ -490,6 +490,7 @@ declare namespace PptxGenJS {
 		lineIdx?: number
 		lineSize?: number
 		lineSpacing?: number
+		margin?: number
 		outline?: { color: Color; size: number }
 		paraSpaceAfter?: number
 		paraSpaceBefore?: number
