6f86c7b222ede655c82b4462815e4ecb1ce9b978
Brent Ely <gitbrent@gmail.com>
2020-01-27 23:46:29 -0600

    updated to v3.1.1

--- src/pptxgen.ts
+++ src/pptxgen.ts
@@ -53,7 +53,7 @@ import * as genMedia from './gen-media'
 import * as genTable from './gen-tables'
 import * as genXml from './gen-xml'
 
-const VERSION = '3.2.0-beta'
+const VERSION = '3.1.1-beta'
 
 export default class PptxGenJS {
 	// Property getters/setters
