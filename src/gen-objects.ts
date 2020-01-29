2d2a85f7c0b1e471148dd2bf8237752f4adc98b6
Brent Ely <gitbrent@gmail.com>
2020-01-28 22:34:09 -0600

    Fixed: regression bug with placeholders

--- src/gen-objects.ts
+++ src/gen-objects.ts
@@ -75,7 +75,7 @@ export function createSlideObject(slideDef: ISlideMasterOptions, target: ISlideL
 				object[key].options.placeholderType = object[key].options.type
 				delete object[key].options.type // remap name for earier handling internally
 				object[key].options.placeholderIdx = 100 + idx
-				if (object[key].text) addTextDefinition(tgt, object[key].text, object[key].options, true)
+				addTextDefinition(tgt, object[key].text, object[key].options, true)
 				// TODO: ISSUE#599 - only text is suported now (add more below)
 				//else if (object[key].image) addImageDefinition(tgt, object[key].image)
 				/* 20200120: So... image placeholders go into the "slideLayoutN.xml" file and addImage doesnt do this yet...
