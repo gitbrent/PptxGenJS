2aa33d22aa30127832453b97a3f09f49a32d7cb9
Brent Ely <gitbrent@gmail.com>
2020-01-29 22:53:09 -0600

    updated for v3.1.1

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for pptxgenjs 3.1.0
+// Type definitions for pptxgenjs 3.1.1
 // Project: https://gitbrent.github.io/PptxGenJS/
 // Definitions by: Brent Ely <https://github.com/gitbrent/>
 //                 Michael Beaumont <https://github.com/michaelbeaumont>
@@ -1127,73 +1127,52 @@ declare namespace PptxGenJS {
 		hidden: boolean
 		slideNumber: ISlideNumber
 		/**
-		 * Generate the chart based on input data.
-		 * @see OOXML Chart Spec: ISO/IEC 29500-1:2016(E)
-		 * @param {CHART_TYPE_NAMES|IChartMulti[]} `type` - chart type
-		 * @param {object[]} data - a JSON object with follow the following format
-		 * @param {IChartOpts} options - chart options
-		 * @example
-		 * {
-		 *   title: 'eSurvey chart',
-		 *   data: [
-		 *		{
-		 *			name: 'Income',
-		 *			labels: ['2005', '2006', '2007', '2008', '2009'],
-		 *			values: [23.5, 26.2, 30.1, 29.5, 24.6]
-		 *		},
-		 *		{
-		 *			name: 'Expense',
-		 *			labels: ['2005', '2006', '2007', '2008', '2009'],
-		 *			values: [18.1, 22.8, 23.9, 25.1, 25]
-		 *		}
-		 *	 ]
-		 * }
-		 * @return {Slide} this class
-		 */
-		addChart(type: CHART_NAME | IChartMulti[], data: [], options?: IChartOpts): Slide
-		/**
-		 * Add Image object
-		 * @note: Remote images (eg: "http://whatev.com/blah"/from web and/or remote server arent supported yet - we'd need to create an <img>, load it, then send to canvas
-		 * @see: https://stackoverflow.com/questions/164181/how-to-fetch-a-remote-image-to-display-in-a-canvas)
-		 * @param {IImageOpts} options - image options
-		 * @return {Slide} this class
-		 */
-		addImage(options: IImageOpts): Slide
-		/**
-		 * Add Media (audio/video) object
-		 * @param {IMediaOpts} options - media options
-		 * @return {Slide} this class
-		 */
-		addMedia(options: IMediaOpts): Slide
-		/**
-		 * Add Speaker Notes to Slide
-		 * @docs https://gitbrent.github.io/PptxGenJS/docs/speaker-notes.html
-		 * @param {string} notes - notes to add to slide
-		 * @return {Slide} this class
-		 */
-		addNotes(notes: string): Slide
-		/**
-		 * Add shape object to Slide
-		 * @param {SHAPE_NAME} shape - shape object
-		 * @param {IShapeOptions} options - shape options
-		 * @return {Slide} this class
-		 */
-		addShape(shape: SHAPE_NAME, options?: IShapeOptions): Slide
-		/**
-		 * Add shape object to Slide
-		 * @note can be recursive
-		 * @param {TableRow[]} arrTabRows - table rows
-		 * @param {ITableOptions} options - table options
-		 * @return {Slide} this class
-		 */
-		addTable(arrTabRows: TableRow[], options?: ITableOptions): Slide
-		/**
-		 * Add text object to Slide
-		 * @param {string|IText[]} text - text string or complex object
-		 * @param {ITextOpts} options - text options
-		 * @return {Slide} this class
-		 * @since: 1.0.0
-		 */
-		addText(text: string | IText[], options?: ITextOpts): Slide
+	     * Add chart to Slide
+	     * @param {CHART_NAME|IChartMulti[]} type - chart type
+	     * @param {object[]} data - a JSON object with follow the following format
+	     * @param {IChartOpts} options - chart options
+	     * @return {Slide} this Slide
+	     */
+	    addChart(type: CHART_NAME | IChartMulti[], data: [], options?: IChartOpts): Slide;
+	    /**
+	     * Add image to Slide
+	     * @param {IImageOpts} options - image options
+	     * @return {Slide} this Slide
+	     */
+	    addImage(options: IImageOpts): Slide;
+	    /**
+	     * Add media (audio/video) to Slide
+	     * @param {IMediaOpts} options - media options
+	     * @return {Slide} this Slide
+	     */
+	    addMedia(options: IMediaOpts): Slide;
+	    /**
+	     * Add speaker notes to Slide
+	     * @docs https://gitbrent.github.io/PptxGenJS/docs/speaker-notes.html
+	     * @param {string} notes - notes to add to slide
+	     * @return {Slide} this Slide
+	     */
+	    addNotes(notes: string): Slide;
+	    /**
+	     * Add shape to Slide
+	     * @param {SHAPE_NAME} shapeName - shape name
+	     * @param {IShapeOptions} options - shape options
+	     * @return {Slide} this Slide
+	     */
+	    addShape(shapeName: SHAPE_NAME, options?: IShapeOptions): Slide;
+	    /**
+	     * Add table to Slide
+	     * @param {TableRow[]} tableRows - table rows
+	     * @param {ITableOptions} options - table options
+	     * @return {Slide} this Slide
+	     */
+	    addTable(tableRows: TableRow[], options?: ITableOptions): Slide;
+	    /**
+	     * Add text to Slide
+	     * @param {string|IText[]} text - text string or complex object
+	     * @param {ITextOpts} options - text options
+	     * @return {Slide} this Slide
+	     */
+	    addText(text: string | IText[], options?: ITextOpts): Slide;
 	}
 }
