8e9e046b9ec15ff8379f5169ce926fc7b4496594
Brent Ely <gitbrent@gmail.com>
2020-06-08 22:46:15 -0500

    updated version

--- src/pptxgen.ts
+++ src/pptxgen.ts
@@ -82,7 +82,7 @@ import * as genMedia from './gen-media'
 import * as genTable from './gen-tables'
 import * as genXml from './gen-xml'
 
-const VERSION = '3.3.0-beta-20200607:2247'
+const VERSION = '3.3.0-beta-20200608:2238'
 
 export default class PptxGenJS implements IPresentation {
 	// Property getters/setters
