2ec105d2726f8121f7a4a0c6b128a8c5b90d025b
Brent Ely <gitbrent@gmail.com>
2020-08-16 20:38:31 -0500

    better example format

--- types/index.d.ts
+++ types/index.d.ts
@@ -854,18 +854,15 @@ declare namespace PptxGenJS {
 		/**
 		 * URL or relative path
 		 *
-		 * @example 'https://onedrives.com/myimg.png`
-		 * retrieve image via URL
-		 * @example '/home/gitbrent/images/myimg.png`
-		 * retrieve image via local path
+		 * @example 'https://onedrives.com/myimg.png` // retrieve image via URL
+		 * @example '/home/gitbrent/images/myimg.png` // retrieve image via local path
 		 */
 		path?: string
 		/**
 		 * base64-encoded string
 		 * - Useful for avoiding potential path/server issues
 		 *
-		 * @example 'image/png;base64,iVtDafDrBF[...]='
-		 * adds a pre-encoded image
+		 * @example 'image/png;base64,iVtDafDrBF[...]=' // pre-encoded image in base-64
 		 */
 		data?: string
 	}
