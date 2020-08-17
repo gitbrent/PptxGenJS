ce509064b1475d4d87b240feaaed43f5a81c8c3b
Brent Ely <gitbrent@gmail.com>
2020-08-16 20:36:52 -0500

    better example format

--- types/index.d.ts
+++ types/index.d.ts
@@ -813,47 +813,37 @@ declare namespace PptxGenJS {
 	 * - Inches
 	 * - Percentage
 	 *
-	 * @example 10.25
-	 * coordinate in inches
-	 * @example '75%'
-	 * coordinate as percentage of slide size
+	 * @example 10.25 // coordinate in inches
+	 * @example '75%' // coordinate as percentage of slide size
 	 */
 	export type Coord = number | string
 	export type PositionProps = {
 		/**
 		 * Horizontal position
 		 * - inches or percentage
-		 * @example 10.25
-		 * position in inches
-		 * @example '75%'
-		 * position as percentage of slide size
+		 * @example 10.25 // position in inches
+		 * @example '75%' // position as percentage of slide size
 		 */
 		x?: Coord
 		/**
 		 * Vertical position
 		 * - inches or percentage
-		 * @example 10.25
-		 * position in inches
-		 * @example '75%'
-		 * position as percentage of slide size
+		 * @example 10.25 // position in inches
+		 * @example '75%' // position as percentage of slide size
 		 */
 		y?: Coord
 		/**
 		 * Height
 		 * - inches or percentage
-		 * @example 10.25
-		 * height in inches
-		 * @example '75%'
-		 * height as percentage of slide size
+		 * @example 10.25 // height in inches
+		 * @example '75%' // height as percentage of slide size
 		 */
 		h?: Coord
 		/**
 		 * Width
 		 * - inches or percentage
-		 * @example 10.25
-		 * width in inches
-		 * @example '75%'
-		 * width as percentage of slide size
+		 * @example 10.25 // width in inches
+		 * @example '75%' // width as percentage of slide size
 		 */
 		w?: Coord
 	}
