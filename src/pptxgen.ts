a511cff6f8e04b4561bc10aa5a0c9423ef045c27
Brent Ely <gitbrent@users.noreply.github.com>
2020-12-07 22:02:42 -0600

    typescript 3.9 to 4.1 update

--- src/pptxgen.ts
+++ src/pptxgen.ts
@@ -85,7 +85,7 @@ import * as genMedia from './gen-media'
 import * as genTable from './gen-tables'
 import * as genXml from './gen-xml'
 
-const VERSION = '3.4.0-beta-20201206-2301'
+const VERSION = '3.4.0-beta-20201207-2155'
 
 export default class PptxGenJS implements IPresentationProps {
 	// Property getters/setters
