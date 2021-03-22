8282613d63af351a43dca3c8278eeef06c870ab3
Brent Ely <gitbrent@users.noreply.github.com>
2021-03-21 20:43:00 -0500

    WIP

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -1,24 +1,47 @@
-import Layout from "@theme/Layout";
 import React from "react";
+import Layout from "@theme/Layout";
+import useThemeContext from "@theme/hooks/useThemeContext";
 import "../css/bootstrap.min.css";
+//import "../css/purged.min.css";
 
 export default () => {
+	const Header = () => {
+		const { isDarkTheme, setLightTheme, setDarkTheme } = useThemeContext();
+		return (
+			<header style={{ backgroundColor: isDarkTheme ? "var(--bs-gray-dark)" : "var(--bs-light)" }}>
+				<div className="container">
+					<div className="row justify-content-center">
+						<div className="col-auto">
+							<div className="my-5">
+								<h1 className={`display-1 mb-4 ${isDarkTheme ? "text-light" : ""}`}>PptxGenJS</h1>
+								<h3 className={`fw-light mb-2 ${isDarkTheme ? "text-muted" : "text-muted"}`}>Create JavaScript PowerPoint Presentations</h3>
+								<h6 className="text-muted fw-light mb-4">The fastest growing, second most starred powerpoint library on GitHub</h6>
+								<div className="text-muted fw-light mb-4 fw-light">The fastest growing, 2nd most starred powerpoint library on GitHub</div>
+								<div className="row row-cols-1 row-cols-md-2 g-4">
+									<div className="col">
+										<button type="button" className={`w-100 btn ${isDarkTheme ? "btn-outline-light" : "btn-outline-dark"}`}>
+											Get Started
+										</button>
+									</div>
+									<div className="col">
+										<button type="button" className={`w-100 btn ${isDarkTheme ? "btn-outline-light" : "btn-outline-dark"}`}>
+											Features
+										</button>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</header>
+		);
+	};
+
 	return (
 		<Layout title="pptxgenjs home">
-			<div className="container">
-				<header className="bg-light">
-					<div className="row my-5">
-						<div className="col text-center">
-							<h1 className="display-1">pptxgenjs</h1>
-						</div>
-					</div>
-					<div className="row my-3">
-						<div className="col text-center">
-							<h4>Fastest growing powerpoint library</h4>
-						</div>
-					</div>
-				</header>
-				<main>
+			<div>
+				<Header />
+				<main className="container">
 					<div className="row row-cols-2 row-cols-md-4 g-4">
 						<div className="col">COL1</div>
 						<div className="col">COL2</div>
