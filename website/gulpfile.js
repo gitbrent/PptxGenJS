0751bd8a13808dd5ec73436c62620075e0420528
Brent Ely <gitbrent@users.noreply.github.com>
2021-03-22 22:20:29 -0500

    new build

--- website/gulpfile.js
+++ website/gulpfile.js
@@ -6,11 +6,20 @@
 let gulp = require("gulp");
 
 // TASKS: Deploy
+gulp.task("deploy-assets", () => {
+	return gulp.src("./build/assets/*/*").pipe(gulp.dest("../assets"));
+});
 gulp.task("deploy-docs", () => {
 	return gulp.src("./build/docs/**").pipe(gulp.dest("../docs/"));
 });
 gulp.task("deploy-html", () => {
 	return gulp.src("./build/*.html").pipe(gulp.dest("../"));
+});
+gulp.task("deploy-license", () => {
+	return gulp.src("./build/license**/**").pipe(gulp.dest("../"));
+});
+gulp.task("deploy-privacy", () => {
+	return gulp.src("./build/privacy/**/**").pipe(gulp.dest("../"));
 });
 gulp.task("deploy-img", () => {
 	return gulp.src("./build/img/*.*").pipe(gulp.dest("../img/"));
@@ -20,6 +29,6 @@ gulp.task("deploy-sitemap", () => {
 });
 
 // Build/Deploy
-gulp.task("default", gulp.parallel("deploy-docs", "deploy-html", "deploy-img", "deploy-sitemap"), () => {
+gulp.task("default", gulp.parallel("deploy-assets", "deploy-docs", "deploy-html", "deploy-license", "deploy-privacy", "deploy-img", "deploy-sitemap"), () => {
 	console.log("Done");
 });
