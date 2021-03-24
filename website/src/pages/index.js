c8ebb4f4e26a04f500cbd92091e8dd6ebcac443b
Brent Ely <gitbrent@users.noreply.github.com>
2021-03-23 23:31:50 -0500

    better content!

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -14,8 +14,8 @@ export default () => {
 						<div className="col-auto">
 							<div className="my-5">
 								<h1 className={`display-1 mb-3 ${isDarkTheme ? "text-light" : ""}`}>PptxGenJS</h1>
-								<h3 className={`fw-light mb-4 ${isDarkTheme ? "text-muted" : "text-muted"}`}>Create JavaScript PowerPoint Presentations</h3>
-								<h6 className="text-muted fw-light mb-4">The most forked javascript+powerpoint project on GitHub with nearly 1,000 stars</h6>
+								<h3 className={`fw-light mb-4 ${isDarkTheme ? "text-muted" : "text-muted"}`}>Create PowerPoint presentations with JavaScript</h3>
+								<h6 className="text-muted fw-light mb-4">The most popular powerpoint library on npm with nearly 1,000 stars on GitHub</h6>
 								<div className="row row-cols-1 row-cols-md-2 g-4 my-0">
 									<div className="col">
 										<button
@@ -53,11 +53,12 @@ export default () => {
 						<div className="col-12 col-md-6">
 							<div className={`card h-100 ${isDarkTheme ? "bg-secondary text-white" : "bg-light"}`}>
 								<div className="card-header">
-									<h5 className="fw-light mb-0">Works Everywhere</h5>
+									<h5 className="fw-light mb-0">Works Everwhere</h5>
 								</div>
 								<div className={`card-body ${isDarkTheme ? "bg-dark" : "bg-white"}`}>
 									<ul className="mb-0">
 										<li>Every modern desktop and mobile browser is supported</li>
+										<li>Integrates with Node, Angular, React and Electron</li>
 										<li>IE11 support available via polyfill</li>
 									</ul>
 								</div>
@@ -70,8 +71,9 @@ export default () => {
 								</div>
 								<div className={`card-body ${isDarkTheme ? "bg-dark" : "bg-white"}`}>
 									<ul className="mb-0">
-										<li>All major object types are available (charts, tables, etc.)</li>
-										<li>Master Slides for corporate branding are supported</li>
+										<li>All major object types are available (charts, shapes, tables, etc.)</li>
+										<li>Master Slides for academic/corporate branding</li>
+										<li>SVG images, animated gifs, YouTube videos, RTL text, and Asian fonts</li>
 									</ul>
 								</div>
 							</div>
@@ -79,13 +81,13 @@ export default () => {
 						<div className="col-12 col-md-6">
 							<div className={`card h-100 ${isDarkTheme ? "bg-secondary text-white" : "bg-light"}`}>
 								<div className="card-header">
-									<h5 className="fw-light mb-0">Simple To Use</h5>
+									<h5 className="fw-light mb-0">Simple And Powerful</h5>
 								</div>
 								<div className={`card-body ${isDarkTheme ? "bg-dark" : "bg-white"}`}>
 									<ul className="mb-0">
-										<li>The easiest PowerPoint library to use</li>
-										<li>Create a presentation using just a few lines of code</li>
+										<li>The absolute easiest PowerPoint library to use</li>
 										<li>Learn as you code will full typescript definitions included</li>
+										<li>Tons of demo code comes included (almost 80 slides of features)</li>
 									</ul>
 								</div>
 							</div>
@@ -93,13 +95,13 @@ export default () => {
 						<div className="col-12 col-md-6">
 							<div className={`card h-100 ${isDarkTheme ? "bg-secondary text-white" : "bg-light"}`}>
 								<div className="card-header">
-									<h5 className="fw-light mb-0">Modern</h5>
+									<h5 className="fw-light mb-0">Export Your Way</h5>
 								</div>
 								<div className={`card-body ${isDarkTheme ? "bg-dark" : "bg-white"}`}>
 									<ul className="mb-0">
-										<li>Integrates with client browsers, Node, Angular, React and Electron</li>
-										<li>Export files direct to browser, blob, stream and more</li>
-										<li>Presentation compression supported</li>
+										<li>Exports files direct to client browsers with proper MIME-type</li>
+										<li>Other export formats available: base64, blob, stream, etc.</li>
+										<li>Presentation compression options and more</li>
 									</ul>
 								</div>
 							</div>
