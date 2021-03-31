f3b32f2a753de53eb7e3b82aeffce4a56e4cee18
Brent Ely <gitbrent@users.noreply.github.com>
2021-03-31 17:50:40 -0500

    updated for v3.5.0

---
+++ demo-react/precache-manifest.d0f81ec3469626409049a9f093a32c78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b08453105f1a8b6a98fa7af80d405ae",
+    "url": "/PptxGenJS/demo-react/index.html"
+  },
+  {
+    "revision": "5f82b5830f683e6136d6",
+    "url": "/PptxGenJS/demo-react/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "6496659f2b34c615c5bb",
+    "url": "/PptxGenJS/demo-react/static/js/2.afe923fa.chunk.js"
+  },
+  {
+    "revision": "5f82b5830f683e6136d6",
+    "url": "/PptxGenJS/demo-react/static/js/main.c3f5c8ee.chunk.js"
+  },
+  {
+    "revision": "1cd1f1736ea26c762ae7",
+    "url": "/PptxGenJS/demo-react/static/js/runtime-main.41ac10bb.js"
+  },
+  {
+    "revision": "bcad93aedc1415f5c6f562983a74d71d",
+    "url": "/PptxGenJS/demo-react/static/media/logo.bcad93ae.svg"
+  }
+]);
