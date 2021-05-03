557fc6d5a8cf2fc50e13b2b398f7bc56e187244c
Brent Ely <gitbrent@users.noreply.github.com>
2021-05-03 00:00:32 -0500

    updated to v3.7.0-beta

--- src/pptxgen.ts
+++ src/pptxgen.ts
@@ -97,7 +97,7 @@ import * as genMedia from './gen-media'
 import * as genTable from './gen-tables'
 import * as genXml from './gen-xml'
 
-const VERSION = '3.6.0'
+const VERSION = '3.7.0-beta-20210503-0001'
 
 export default class PptxGenJS implements IPresentationProps {
 	// Property getters/setters
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for pptxgenjs 3.6.0
+// Type definitions for pptxgenjs 3.7.0
 // Project: https://gitbrent.github.io/PptxGenJS/
 // Definitions by: Brent Ely <https://github.com/gitbrent/>
 //                 Michael Beaumont <https://github.com/michaelbeaumont>
