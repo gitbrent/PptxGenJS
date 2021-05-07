ddb4365ffc4a16a0ede3cfe1fcb1b0b9454b4465
Brent Ely <gitbrent@users.noreply.github.com>
2021-05-06 21:36:33 -0500

    added CRO coin

--- website/src/pages/sponsor.js
+++ website/src/pages/sponsor.js
@@ -7,6 +7,7 @@ import "../css/purged.css";
 const ADDRESS_BTC = "bc1qm8cqunm00wtxe7eztspfkaysc6n7fedrsvfv6c";
 const ADDRESS_ETH = "0x8F130f0522c9E185E0692B0B2801295732951bAA";
 const ADDRESS_DOT = "112BhnEjhsVPEDPtLqx2pKjuZmu64GDxFHLaxdbqwpu5h8ME";
+const ADDRESS_CRO = "cro1zlgepgq383k43auqha5lp35j8y0jdtku2dm3t6";
 
 export default () => {
 	const PageHeader = () => {
@@ -82,19 +83,19 @@ export default () => {
 					</div>
 					<div className="col-12 col-md-4">
 						<div className={`text-center h-100 p-4 ${isDarkTheme ? "bg-black" : "bg-white"}`}>
-							<h4 className="mb-4">Polkadot (DOT)</h4>
+							<h4 className="mb-4">Crypto.com (CRO)</h4>
 							<p>
-								<img src="/PptxGenJS/img/sponsor_dot.png" alt="polkadot wallet address" />
+								<img src="/PptxGenJS/img/sponsor_cro.png" alt="crypto.com wallet address" />
 							</p>
 							<div className="row justify-content-center align-items-center g-3">
-								<div className="col-auto font-monospace">{ADDRESS_DOT}</div>
+								<div className="col-auto font-monospace">{ADDRESS_CRO}</div>
 								<div className="col-auto">
 									<button
 										type="button"
 										title="click to copy"
 										class="btn btn-sm btn-primary"
 										onClick={() => {
-											navigator.clipboard.writeText(ADDRESS_DOT);
+											navigator.clipboard.writeText(ADDRESS_CRO);
 										}}
 									>
 										Copy
