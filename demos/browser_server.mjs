e5f140ccd7143313f059524b9e5d95d523c3f5cd
Brent Ely <gitbrent@users.noreply.github.com>
2021-06-02 20:40:12 -0500

    added auto-open of demo url

--- demos/browser_server.mjs
+++ demos/browser_server.mjs
@@ -5,12 +5,17 @@
  * REQS: express
  * USAGE: `node browser_server.mjs`
  * AUTH: Brent Ely (https://github.com/gitbrent/)
- * DATE: 20210404
+ * DATE: 20210602
  */
+
+// Use `createRequire` as `require` wont work by default in modules
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
 
 import express from "express";
 const app = express();
 const port = 8000;
+const DEMO_URL = `http://localhost:${port}/browser/index.html`;
 
 app.use("/browser", express.static("./browser"));
 app.use("/common", express.static("./common"));
@@ -18,7 +23,10 @@ app.use("/modules", express.static("./modules"));
 
 app.listen(port, () => {
 	console.log(`\n----------------------==~==~==~==[ SERVER RUNNING ]==~==~==~==----------------------\n`);
-	console.log(`The pptxgenjs browser demo is now live at: http://localhost:${port}/browser/index.html\n`);
+	console.log(`The pptxgenjs browser demo is now live at: ${DEMO_URL}\n`);
 	console.log(`(Press Ctrl-C to stop)`);
 	console.log(`\n----------------------==~==~==~==[ SERVER RUNNING ]==~==~==~==----------------------\n`);
 });
+
+let start = process.platform == "darwin" ? "open" : process.platform == "win32" ? "start" : "xdg-open";
+require("child_process").exec(start + " " + DEMO_URL);
