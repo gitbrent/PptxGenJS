dd7370a5819f22ad38ac18653aac2e416afadfd8
Brent Ely <gitbrent@users.noreply.github.com>
2021-08-21 20:32:45 -0500

    comments

--- src/gen-utils.ts
+++ src/gen-utils.ts
@@ -84,8 +84,8 @@ export function encodeXmlEntities(xml: string): string {
  * @returns {number} EMU value
  */
 export function inch2Emu(inches: number | string): number {
-	// FIRST: Provide Caller Safety: Numbers may get conv<->conv during flight, so be kind and do some simple checks to ensure inches were passed
-	// Any value over 100 damn sure isnt inches, must be EMU already, so just return it
+	// NOTE: Provide Caller Safety: Numbers may get conv<->conv during flight, so be kind and do some simple checks to ensure inches were passed
+	// Any value over 100 damn sure isnt inches, so lets assume its in EMU already, therefore, just return the same value
 	if (typeof inches === 'number' && inches > 100) return inches
 	if (typeof inches === 'string') inches = Number(inches.replace(/in*/gi, ''))
 	return Math.round(EMU * inches)
