7afd9514c28eaad1c64d451d439de49722405314
Brent Ely <gitbrent@users.noreply.github.com>
2021-09-07 21:55:13 -0500

    Done: Introduction tab

--- demos/browser/js/browser.js
+++ demos/browser/js/browser.js
@@ -10,7 +10,10 @@ const SVG_CHECK_CIRCLE =
 	'<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/><path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/></svg>';
 const SVG_CHECK_CIRCLE_FILL =
 	'<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/></svg>';
-
+const SVG_INFO_CIRCLE_FILL =
+	'<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16"><path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z" /></svg>';
+const SVG_INFO_CIRCLE =
+	'<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/><path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/></svg>';
 // ==================================================================================================================
 
 export function doAppStart() {
@@ -23,13 +26,13 @@ export function doAppStart() {
 		return;
 	}
 
-	// STEP 1: Set UI (if you're me :-)
+	// STEP 1: Set UI to dev mode (if you're running locally, congrats you're a dev!)
 	if (window.location.href.indexOf("http://localhost:8000/") > -1) {
-		$("#tab1sec1").click();
-		$("#tab1sec2").click();
+		document.getElementById("basicPres").classList.add("d-none");
+		document.getElementById("codeSandbox").classList.remove("d-none");
 	}
 
-	// STEP 2: Show library info
+	// STEP 2: Introduction tab: Librayr Info
 	{
 		if (typeof Promise !== "function") {
 			$("header").after(
@@ -37,24 +40,24 @@ export function doAppStart() {
 			);
 		} else {
 			let pptx = new PptxGenJS();
-			$("#infoBar").append(`<div class="col text-primary">${SVG_CHECK_CIRCLE_FILL} Version: <span>${pptx.version}</span></div>`);
-			$("#infoBar").append(
-				`<div class='col-auto text-success text-nowrap'>` +
-					`<span style='cursor:help' title='${Object.keys(pptx.ChartType).join(" | ")}'>` +
-					`${SVG_CHECK_CIRCLE_FILL} pptx.ChartType = ${Object.keys(pptx.ChartType).length}` +
-					`</span></div>`
+
+			$("#infoLbl_PptxVers").prepend(`<span class="cursor-help me-1" title="${pptx.version}">${SVG_INFO_CIRCLE}</span>`);
+			$("#infoBox_PptxVers").val(pptx.version);
+			//
+			$("#infoLbl_ChartType").prepend(
+				`<span class="cursor-help me-1" title="${Object.keys(pptx.ChartType).join("; ")}">${SVG_INFO_CIRCLE}</span>`
 			);
-			$("#infoBar").append(
-				`<div class='col-auto text-success text-nowrap'>` +
-					`<span style='cursor:help' title='${Object.keys(pptx.SchemeColor).join(" | ")}'>` +
-					`${SVG_CHECK_CIRCLE_FILL} pptx.SchemeColor = ${Object.keys(pptx.SchemeColor).length}` +
-					`</span></div>`
+			$("#infoBox_ChartType").val(Object.keys(pptx.ChartType).length);
+			//
+			$("#infoLbl_ShapeType").prepend(
+				`<span class="cursor-help me-1" title="${Object.keys(pptx.ShapeType).join("; ")}">${SVG_INFO_CIRCLE}</span>`
 			);
-			$("#infoBar").append(
-				`<div class="col-auto text-success text-nowrap">` +
-					`<span>${SVG_CHECK_CIRCLE_FILL} pptx.ShapeType = ${Object.keys(pptx.ShapeType).length}` +
-					`</span></div>`
+			$("#infoBox_ShapeType").val(Object.keys(pptx.ShapeType).length);
+			//
+			$("#infoLbl_SchemeColor").prepend(
+				`<span class="cursor-help me-1" title="${Object.keys(pptx.SchemeColor).join("; ")}">${SVG_INFO_CIRCLE}</span>`
 			);
+			$("#infoBox_SchemeColor").val(Object.keys(pptx.SchemeColor).length);
 		}
 	}
 
