eb4a376dfbadb16de32ccf3ee46562602bf56d18
Brent Ely <gitbrent@users.noreply.github.com>
2021-09-11 15:37:01 -0500

    New build for v3.8.0-beta

--- dist/pptxgen.es.js
+++ dist/pptxgen.es.js
@@ -1,4 +1,4 @@
-/* PptxGenJS 3.7.1 @ 2021-08-29T21:45:03.228Z */
+/* PptxGenJS 3.7.1 @ 2021-09-11T20:35:32.580Z */
 import JSZip from 'jszip';
 
 /*! *****************************************************************************
@@ -6383,7 +6383,7 @@ function createSvgPngPreview(rel) {
  *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  *  SOFTWARE.
  */
-var VERSION = '3.8.0-beta-20210829-1643';
+var VERSION = '3.8.0-beta-20210911-1530';
 var PptxGenJS = /** @class */ (function () {
     function PptxGenJS() {
         var _this = this;
@@ -6478,20 +6478,8 @@ var PptxGenJS = /** @class */ (function () {
             eleLink.dataset.interception = 'off'; // @see https://docs.microsoft.com/en-us/sharepoint/dev/spfx/hyperlinking
             document.body.appendChild(eleLink);
             // STEP 2: Download file to browser
-            // DESIGN: Use `createObjectURL()` (or MS-specific func for IE11) to D/L files in client browsers (FYI: synchronously executed)
-            if (window.navigator.msSaveOrOpenBlob) {
-                // @see https://docs.microsoft.com/en-us/microsoft-edge/dev-guide/html5/file-api/blob
-                var blob_1 = new Blob([blobContent], { type: 'application/vnd.openxmlformats-officedocument.presentationml.presentation' });
-                eleLink.onclick = function () {
-                    window.navigator.msSaveOrOpenBlob(blob_1, exportName);
-                };
-                eleLink.click();
-                // Clean-up
-                document.body.removeChild(eleLink);
-                // Done
-                return Promise.resolve(exportName);
-            }
-            else if (window.URL.createObjectURL) {
+            // DESIGN: Use `createObjectURL()` to D/L files in client browsers (FYI: synchronously executed)
+            if (window.URL.createObjectURL) {
                 var url_1 = window.URL.createObjectURL(new Blob([blobContent], { type: 'application/vnd.openxmlformats-officedocument.presentationml.presentation' }));
                 eleLink.href = url_1;
                 eleLink.download = exportName;
