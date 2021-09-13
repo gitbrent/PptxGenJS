b8cc2fafb9028cad3e6a7ecd66fdcb2221e7f222
Brent Ely <gitbrent@users.noreply.github.com>
2021-09-12 22:49:59 -0500

    updated page CSS

--- src/pages/html2pptx.js
+++ src/pages/html2pptx.js
@@ -1,96 +1,127 @@
 import React from "react";
 import Layout from "@theme/Layout";
-import useThemeContext from "@theme/hooks/useThemeContext";
 import Gist from "react-gist";
 //import "../css/bootstrap-yeti.css";
 import "../css/purged.css";
 
 export default () => {
 	const Header = () => {
-		const { isDarkTheme, setLightTheme, setDarkTheme } = useThemeContext();
-
 		return (
-			<section className={`mb-5 p-4 ${isDarkTheme ? "bg-black" : "bg-white"}`}>
-				<h4>Table to Slides Feature</h4>
-				<p>Create a presentation from an HTML table with a single line of code. Creates slides as needed (auto-paging).</p>
-				<div className="row align-items-center">
-					<div className="col-auto">
-						<img className="d-none d-md-none d-lg-block border border-light" alt="input: html table" src="/PptxGenJS/img/ex-html-to-powerpoint-1.png" height="500" />
-						<img className="d-none d-md-block d-lg-none border border-light" alt="input: html table" src="/PptxGenJS/img/ex-html-to-powerpoint-1.png" height="300" />
-						<img className="d-block d-md-none d-lg-none border border-light" alt="input: html table" src="/PptxGenJS/img/ex-html-to-powerpoint-1.png" height="200" />
+			<div className="card useTheme mb-5">
+				<div className="card-header h4 bg-primary text-white">About</div>
+				<div className="card-body">
+					<p>Create a presentation from an HTML table with a single line of code.</p>
+					<p>
+						The <code>tableToSlides</code> method clones the table including CSS style and creates slides as needed (auto-paging).
+					</p>
+					<div className="row align-items-center my-3">
+						<div className="col-auto">
+							<img
+								className="d-none d-md-none d-lg-block border border-light"
+								alt="input: html table"
+								src="/PptxGenJS/img/ex-html-to-powerpoint-1.png"
+								height="500"
+							/>
+							<img
+								className="d-none d-md-block d-lg-none border border-light"
+								alt="input: html table"
+								src="/PptxGenJS/img/ex-html-to-powerpoint-1.png"
+								height="300"
+							/>
+							<img
+								className="d-block d-md-none d-lg-none border border-light"
+								alt="input: html table"
+								src="/PptxGenJS/img/ex-html-to-powerpoint-1.png"
+								height="200"
+							/>
+						</div>
+						<div className="col-auto col-md text-center">
+							<h1 className="mb-0">→</h1>
+						</div>
+						<div className="col-auto">
+							<img
+								className="d-none d-md-none d-lg-block border border-light"
+								alt="output: powerpoint slides"
+								src="/PptxGenJS/img/ex-html-to-powerpoint-2.png"
+								height="500"
+							/>
+							<img
+								className="d-none d-md-block d-lg-none border border-light"
+								alt="output: powerpoint slides"
+								src="/PptxGenJS/img/ex-html-to-powerpoint-2.png"
+								height="300"
+							/>
+							<img
+								className="d-block d-md-none d-lg-none border border-light"
+								alt="output: powerpoint slides"
+								src="/PptxGenJS/img/ex-html-to-powerpoint-2.png"
+								height="200"
+							/>
+						</div>
 					</div>
-					<div className="col-auto col-md text-center">
-						<h1 className="mb-0">→</h1>
-					</div>
-					<div className="col-auto">
-						<img
-							className="d-none d-md-none d-lg-block border border-light"
-							alt="output: powerpoint slides"
-							src="/PptxGenJS/img/ex-html-to-powerpoint-2.png"
-							height="500"
-						/>
-						<img
-							className="d-none d-md-block d-lg-none border border-light"
-							alt="output: powerpoint slides"
-							src="/PptxGenJS/img/ex-html-to-powerpoint-2.png"
-							height="300"
-						/>
-						<img
-							className="d-block d-md-none d-lg-none border border-light"
-							alt="output: powerpoint slides"
-							src="/PptxGenJS/img/ex-html-to-powerpoint-2.png"
-							height="200"
-						/>
-					</div>
+					<p className="mb-0">
+						Refer to the HTML-to-PowerPoint docs for a complete list of options like repeating header, start location on subsequnet slides, character and line weight,
+						etc.
+					</p>
 				</div>
-			</section>
+				<div className="card-footer">
+					<button
+						type="button"
+						aria-label="documentation"
+						className="btn btn-outline-primary px-5"
+						onClick={() => (window.location.href = "/PptxGenJS/docs/html-to-powerpoint")}
+					>
+						HTML to PowerPoint Docs
+					</button>
+				</div>
+			</div>
 		);
 	};
 
 	const Body = () => {
-		const { isDarkTheme, setLightTheme, setDarkTheme } = useThemeContext();
-
 		// NOTE: 20210407: cant use `row-cols-12` yet as docusaurus core bootstrap is ruining `rows` style, use "col-12" etc on cols to supercede for now
 		return (
-			<section className={`mb-5 p-4 ${isDarkTheme ? "bg-black" : "bg-white"}`}>
-				<h4>Demo Code</h4>
-				<p>
-					The <code>tableToSlides</code> method clones the table including CSS style and creates slides as needed (auto-paging). These 3 lines of code were all it took to
-					produce the slides shown above!
-				</p>
-				<p>View the HTML-to-PowerPoint docs for a complete list of options like repeating header, start location on subsequnet slides, character and line weight, etc.</p>
-				<Gist id="494850b6775dd5c8ce314672a1846208" />
-				<button
-					type="button"
-					aria-label="HTML to PowerPoint Documentation"
-					className={`w-100 mt-4 btn ${isDarkTheme ? "btn-outline-primary" : "btn-outline-primary"}`}
-					onClick={() => (window.location.href = "/PptxGenJS/docs/html-to-powerpoint")}
-				>
-					HTML to PowerPoint Docs
-				</button>
-			</section>
+			<div className="card useTheme mb-5">
+				<div className="card-header h4 bg-primary text-white">Code</div>
+				<div className="card-body">
+					<p>These 3 lines of code were all it took to produce the slides shown above!</p>
+					<Gist id="494850b6775dd5c8ce314672a1846208" />
+				</div>
+				<div className="card-footer">
+					<button
+						type="button"
+						aria-label="documentation"
+						className="btn btn-outline-primary px-5"
+						onClick={() => (window.location.href = "/PptxGenJS/docs/html-to-powerpoint")}
+					>
+						HTML to PowerPoint Docs
+					</button>
+				</div>
+			</div>
 		);
 	};
 
 	const Footer = () => {
-		const { isDarkTheme, setLightTheme, setDarkTheme } = useThemeContext();
-
 		return (
-			<section className={`mb-5 p-4 ${isDarkTheme ? "bg-black" : "bg-white"}`}>
-				<h4>Live Demo</h4>
-				<p>Try it for yourself! The demo below has an interactive table plus various options to test drive.</p>
-				<div className="text-center">
-					<img alt="HTML Table" src="/PptxGenJS/img/ex-html-to-powerpoint-3.png" height="500" className="border border-light" />
+			<div className="card useTheme mb-5">
+				<div className="card-header h4 bg-primary text-white">Demo</div>
+				<div className="card-body">
+					<p>Try it for yourself! The demo below has an interactive table plus various options to test drive.</p>
+					<div className="text-center">
+						<img alt="HTML Table" src="/PptxGenJS/img/ex-html-to-powerpoint-3.png" height="500" className="border border-light" />
+					</div>
 				</div>
-				<button
-					type="button"
-					aria-label="HTML to PowerPoint Demo"
-					className={`w-100 mt-4 btn ${isDarkTheme ? "btn-outline-primary" : "btn-outline-primary"}`}
-					onClick={() => (window.location.href = "/PptxGenJS/demo/index.html#html2pptx")}
-				>
-					HTML to PowerPoint Demo
-				</button>
-			</section>
+				<div className="card-footer">
+					<button
+						type="button"
+						aria-label="demo"
+						className="btn btn-outline-primary px-5"
+						onClick={() => (window.location.href = "/PptxGenJS/demo/index.html#html2pptx")}
+					>
+						HTML to PowerPoint Demo
+					</button>
+				</div>
+			</div>
 		);
 	};
 
