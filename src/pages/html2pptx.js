870a36d33dd142e3a1d809f408174a657ce1dc1c
Brent Ely <gitbrent@users.noreply.github.com>
2021-09-27 21:47:01 -0500

    Updated: lots

--- src/pages/html2pptx.js
+++ src/pages/html2pptx.js
@@ -7,87 +7,60 @@ import "../css/purged.css";
 export default () => {
 	const Header = () => {
 		return (
-			<div className="card useTheme">
-				<div className="card-body">
-					<h3 className="mb-3">About HTML-to-PPTX</h3>
-					<p>
-						The <code>tableToSlides</code> method generates a presentation from an HTML table element id.
-					</p>
-					<ul>
-						<li>Many options are available including repeating header, start location on subsequent slides, character and line weight</li>
-						<li>Additional slides are automatically created as needed (auto-paging)</li>
-						<li>The table's style (CSS) is copied into the PowerPoint table</li>
-					</ul>
-					<div className="d-none d-md-flex row align-items-center justify-content-center my-3">
-						<div className="col-auto">
-							<img
-								className="d-none d-md-none d-lg-block border border-light"
-								alt="input: html table"
-								src="/PptxGenJS/img/ex-html-to-powerpoint-1.png"
-								height="400"
-							/>
-							<img
-								className="d-none d-md-block d-lg-none border border-light"
-								alt="input: html table"
-								src="/PptxGenJS/img/ex-html-to-powerpoint-1.png"
-								height="300"
-							/>
-							<img
-								className="d-block d-md-none d-lg-none border border-light"
-								alt="input: html table"
-								src="/PptxGenJS/img/ex-html-to-powerpoint-1.png"
-								height="200"
-							/>
-						</div>
-						<div className="col-auto">
-							<h1 className="mb-0">→</h1>
-						</div>
-						<div className="col-auto">
-							<img
-								className="d-none d-md-none d-lg-block border border-light"
-								alt="output: powerpoint slides"
-								src="/PptxGenJS/img/ex-html-to-powerpoint-2.png"
-								height="400"
-							/>
-							<img
-								className="d-none d-md-block d-lg-none border border-light"
-								alt="output: powerpoint slides"
-								src="/PptxGenJS/img/ex-html-to-powerpoint-2.png"
-								height="300"
-							/>
-							<img
-								className="d-block d-md-none d-lg-none border border-light"
-								alt="output: powerpoint slides"
-								src="/PptxGenJS/img/ex-html-to-powerpoint-2.png"
-								height="200"
-							/>
-						</div>
+			<section className="bgTheme p-4">
+				<h3 className="mb-3">About HTML-to-PPTX</h3>
+				<p>
+					The <code>tableToSlides</code> method generates a presentation from an HTML table element id.
+				</p>
+				<ul>
+					<li>Many options are available including repeating header, start location on subsequent slides, character and line weight</li>
+					<li>Additional slides are automatically created as needed (auto-paging)</li>
+					<li>The table's style (CSS) is copied into the PowerPoint table</li>
+				</ul>
+				<div className="d-none d-md-flex row align-items-center justify-content-center my-3">
+					<div className="col-auto">
+						<img className="d-none d-md-none d-lg-block border border-light" alt="input: html table" src="/PptxGenJS/img/ex-html-to-powerpoint-1.png" height="400" />
+						<img className="d-none d-md-block d-lg-none border border-light" alt="input: html table" src="/PptxGenJS/img/ex-html-to-powerpoint-1.png" height="300" />
+						<img className="d-block d-md-none d-lg-none border border-light" alt="input: html table" src="/PptxGenJS/img/ex-html-to-powerpoint-1.png" height="200" />
+					</div>
+					<div className="col-auto">
+						<h1 className="mb-0">→</h1>
+					</div>
+					<div className="col-auto">
+						<img
+							className="d-none d-md-none d-lg-block border border-light"
+							alt="output: powerpoint slides"
+							src="/PptxGenJS/img/ex-html-to-powerpoint-2.png"
+							height="400"
+						/>
+						<img
+							className="d-none d-md-block d-lg-none border border-light"
+							alt="output: powerpoint slides"
+							src="/PptxGenJS/img/ex-html-to-powerpoint-2.png"
+							height="300"
+						/>
+						<img
+							className="d-block d-md-none d-lg-none border border-light"
+							alt="output: powerpoint slides"
+							src="/PptxGenJS/img/ex-html-to-powerpoint-2.png"
+							height="200"
+						/>
 					</div>
 				</div>
-				<div className="card-footer">
-					<button
-						type="button"
-						aria-label="documentation"
-						className="btn btn-outline-primary px-5"
-						onClick={() => (window.location.href = "/PptxGenJS/docs/html-to-powerpoint")}
-					>
-						HTML to PowerPoint Docs
-					</button>
-				</div>
-			</div>
+			</section>
 		);
 	};
 
-	const Body = () => {
+	const SampleCode = () => {
 		// NOTE: 20210407: cant use `row-cols-12` yet as docusaurus core bootstrap is ruining `rows` style, use "col-12" etc on cols to supercede for now
 		return (
-			<div className="card useTheme">
+			<div className="card useTheme h-100">
 				<div className="card-body">
 					<h3 className="mb-3">Sample Code</h3>
-					<p>These 3 lines of code were all it took to produce the slides shown above!</p>
+					<p>Reproduce a table in as little as 3 lines of code.</p>
 					<Gist id="494850b6775dd5c8ce314672a1846208" />
 				</div>
-				<div className="card-footer">
+				<div className="card-footer text-center">
 					<button
 						type="button"
 						aria-label="documentation"
@@ -101,17 +74,17 @@ export default () => {
 		);
 	};
 
-	const Footer = () => {
+	const LiveDemo = () => {
 		return (
-			<div className="card useTheme">
+			<div className="card useTheme h-100">
 				<div className="card-body">
-					<h3 className="mb-3">Sample Output</h3>
-					<p>Try the live demo for yourself.</p>
+					<h3 className="mb-3">Live Demo</h3>
+					<p>Try the html-to-pptx feature out for yourself.</p>
 					<div className="text-center">
-						<img alt="HTML Table" src="/PptxGenJS/img/ex-html-to-powerpoint-3.png" height="500" className="border border-light" />
+						<img alt="HTML Table" src="/PptxGenJS/img/ex-html-to-powerpoint-3.png" className="border border-light" />
 					</div>
 				</div>
-				<div className="card-footer">
+				<div className="card-footer text-center">
 					<button
 						type="button"
 						aria-label="demo"
@@ -134,10 +107,10 @@ export default () => {
 						<Header />
 					</div>
 					<div className="col">
-						<Body />
+						<SampleCode />
 					</div>
 					<div className="col">
-						<Footer />
+						<LiveDemo />
 					</div>
 				</div>
 			</div>
