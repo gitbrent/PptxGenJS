76f7727db25bc24428a094889482e9048f9e1395
Brent Ely <gitbrent@users.noreply.github.com>
2021-09-28 21:25:44 -0500

    Updated: new v3.8.0 build

--- demo/react/precache-manifest.5174e0bc4759d8230aed5561a6d97fd5.js
+++ demo/react/precache-manifest.5174e0bc4759d8230aed5561a6d97fd5.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4d942d67b07272f2d85e4c11340c1b52",
+    "revision": "fdefa4ece9c84c454ec3c8c0f3e0ec95",
     "url": "/PptxGenJS/demo/react/index.html"
   },
   {
-    "revision": "118c1062d71838aa0e5e",
+    "revision": "e44353ff26d82c616c83",
     "url": "/PptxGenJS/demo/react/static/css/main.b100e6da.chunk.css"
   },
   {
-    "revision": "9fe8dcfae5fc2c649fdf",
-    "url": "/PptxGenJS/demo/react/static/js/2.cbcf5821.chunk.js"
+    "revision": "66db86d2e74cc248f3df",
+    "url": "/PptxGenJS/demo/react/static/js/2.d556b67d.chunk.js"
   },
   {
-    "revision": "118c1062d71838aa0e5e",
-    "url": "/PptxGenJS/demo/react/static/js/main.1908e6ba.chunk.js"
+    "revision": "e44353ff26d82c616c83",
+    "url": "/PptxGenJS/demo/react/static/js/main.a3f4fe53.chunk.js"
   },
   {
     "revision": "86f79533c3f8be77997f",
