274fc9d30c665af30d352a629a86ae97f4913aed
Mathieu Bruyen <mathieu.bruyen@gpulse.fr>
2021-10-29 10:40:51 +0200

    allow chart legend to specify a manual layout

--- src/core-interfaces.ts
+++ src/core-interfaces.ts
@@ -1295,6 +1295,7 @@ export interface IChartPropsLegend {
 	legendFontFace?: string
 	legendFontSize?: number
 	legendPos?: 'b' | 'l' | 'r' | 't' | 'tr'
+	legendLayout?: PositionProps
 }
 export interface IChartPropsTitle extends TextBaseProps {
 	title?: string
