e802dc55347c387a1767f8926a516099bb17cea5
Brent Ely <gitbrent@users.noreply.github.com>
2021-12-11 12:35:22 -0600

    updated to bootstrap 5

--- demos/react-demo/src/App.js
+++ demos/react-demo/src/App.js
@@ -48,9 +48,9 @@ function App() {
 			w: "80%",
 			h: 1,
 			fontSize: 36,
-			align: 'center',
-			fill: { color:'D3E3F3' },
-			color: '008899',
+			align: "center",
+			fill: { color: "D3E3F3" },
+			color: "008899",
 		});
 
 		slide.addChart(pptx.ChartType.radar, dataChartRadar, { x: 1, y: 1.9, w: 8, h: 3 });
@@ -61,75 +61,64 @@ function App() {
 			w: "100%",
 			h: 0.33,
 			fontSize: 10,
-			align: 'center',
-			fill: 'E1E1E1', //{ color: pptx.SchemeColor.background2 },
-			color: 'A1A1A1' // pptx.SchemeColor.accent3,
+			align: "center",
+			fill: "E1E1E1", //{ color: pptx.SchemeColor.background2 },
+			color: "A1A1A1", // pptx.SchemeColor.accent3,
 		});
 
 		pptx.writeFile({ fileName: "pptxgenjs-demo-react.pptx" });
 	}
 
 	return (
-		<div>
+		<section>
 			<nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-				<a className="navbar-brand" href="https://gitbrent.github.io/PptxGenJS/">
-					<img src={logo} width="30" height="30" className="d-inline-block align-top mr-2" alt="logo" />
-					PptxGenJS
-				</a>
-				<button
-					className="navbar-toggler"
-					type="button"
-					data-toggle="collapse"
-					data-target="#navbarColor01"
-					aria-controls="navbarColor01"
-					aria-expanded="false"
-					aria-label="Toggle navigation"
-				>
-					<span className="navbar-toggler-icon"></span>
-				</button>
-
-				<div className="collapse navbar-collapse" id="navbarColor01">
-					<ul className="navbar-nav mr-auto">
-						<li className="nav-item active">
-							<a className="nav-link" href="https://gitbrent.github.io/PptxGenJS/demo-react/index.html">
-								Home <span className="sr-only">(current)</span>
-							</a>
-						</li>
-					</ul>
-					<form className="form-inline my-2 my-lg-0">
-						<button
-							type="button"
-							className="btn btn-outline-info mx-3 my-2 my-sm-0"
-							onClick={(ev) => {
-								window.open("https://gitbrent.github.io/PptxGenJS/demo/", true);
-							}}
-						>
-							Demo Page
-						</button>
-						<button
-							type="button"
-							className="btn btn-outline-info mx-3 my-2 my-sm-0"
-							onClick={(ev) => {
-								window.open("https://github.com/gitbrent/PptxGenJS", true);
-							}}
-						>
-							GitHub Project
-						</button>
-						<button
-							type="button"
-							className="btn btn-outline-info mx-3 my-2 my-sm-0"
-							onClick={(ev) => {
-								window.open("https://gitbrent.github.io/PptxGenJS/docs/installation.html", true);
-							}}
-						>
-							API Docs
-						</button>
-					</form>
+				<div className="container-fluid">
+					<a className="navbar-brand" href="https://gitbrent.github.io/PptxGenJS/">
+						<img src={logo} width="30" height="24" />
+						PptxGenJS
+					</a>
+					<button
+						className="navbar-toggler"
+						type="button"
+						data-bs-toggle="collapse"
+						data-bs-target="#navbarSupportedContent"
+						aria-controls="navbarSupportedContent"
+						aria-expanded="false"
+						aria-label="Toggle navigation"
+					>
+						<span className="navbar-toggler-icon"></span>
+					</button>
+					<div className="collapse navbar-collapse" id="navbarSupportedContent">
+						<ul className="navbar-nav me-auto mb-2 mb-lg-0">
+							<li className="nav-item">
+								<a className="nav-link active" aria-current="page" href="#">
+									Home
+								</a>
+							</li>
+						</ul>
+						<ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+							<li class="nav-item me-3">
+								<a class="nav-link" href="https://gitbrent.github.io/PptxGenJS/demos/">
+									Library Demos
+								</a>
+							</li>
+							<li class="nav-item me-3">
+								<a class="nav-link" href="https://gitbrent.github.io/PptxGenJS/docs/installation/">
+									API Documentation
+								</a>
+							</li>
+							<li class="nav-item me-3">
+								<a class="nav-link" href="https://github.com/gitbrent/PptxGenJS/">
+									GitHub Project
+								</a>
+							</li>
+						</ul>
+					</div>
 				</div>
 			</nav>
 
 			<main className="container">
-				<div className="jumbotron mt-5">
+				<div className="jumbotron my-5">
 					<h1 className="display-4">React Demo</h1>
 					<p className="lead">Sample React application to demonstrate using the PptxGenJS library as a module.</p>
 					<hr className="my-4" />
@@ -141,7 +130,7 @@ function App() {
 
 					<div className="row">
 						<div className="col">
-							<button type="button" className="btn btn-success w-100 mr-3" onClick={(_ev) => runDemo()}>
+							<button type="button" className="btn btn-success w-100 me-3" onClick={(_ev) => runDemo()}>
 								Run Demo
 							</button>
 						</div>
@@ -158,7 +147,7 @@ function App() {
 					</div>
 
 					<table id="html2ppt" className="table table-dark" style={{ display: "none" }}>
-						<thead>
+						<thead className="table-dark">
 							<tr>
 								<th>col 1</th>
 								<th>col 2</th>
@@ -175,7 +164,7 @@ function App() {
 					</table>
 				</div>
 			</main>
-		</div>
+		</section>
 	);
 }
 
