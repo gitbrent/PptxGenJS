f1686a4313b65c46bb844b0b401c04028b349e38
Brent Ely <gitbrent@users.noreply.github.com>
2022-01-30 15:04:03 -0600

    updated to v3.10.0-beta

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Notes]
 
+## [3.10.0] - 2022-??-??
+
 ## [3.9.0] - 2021-12-11
 
 ### Added
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for pptxgenjs 3.9.0
+// Type definitions for pptxgenjs 3.10.0
 // Project: https://gitbrent.github.io/PptxGenJS/
 // Definitions by: Brent Ely <https://github.com/gitbrent/>
 //                 Michael Beaumont <https://github.com/michaelbeaumont>
