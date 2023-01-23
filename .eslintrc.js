a86bf32e63b32d82d917e6e299b674557d4ea4b2
Brent Ely <gitbrent@users.noreply.github.com>
2023-01-22 22:56:10 -0600

    added demos mjs fields to ignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,7 +20,7 @@ module.exports = {
 		project: ["./tsconfig.json"],
 	},
 	plugins: ["react", "@typescript-eslint"],
-	ignorePatterns: [".eslintrc.js"],
+	ignorePatterns: [".eslintrc.js", "*.mjs"],
 	rules: {
 		"@typescript-eslint/indent": ["error", "tab"],
 		"@typescript-eslint/prefer-nullish-coalescing": 0, // "warn", too many items!
