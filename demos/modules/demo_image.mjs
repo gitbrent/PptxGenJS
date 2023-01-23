75132729affd52fdf41498ed88ea24e1eb9b0da2
Brent Ely <gitbrent@users.noreply.github.com>
2023-01-22 23:39:41 -0600

    WIP

--- demos/modules/demo_image.mjs
+++ demos/modules/demo_image.mjs
@@ -37,7 +37,7 @@ export function genSlides_Image(pptx) {
 function genSlide01(pptx) {
 	let slide = pptx.addSlide({ sectionTitle: "Images" });
 
-	slide.addTable([[{ text: "Image Examples: Misc Image Types", options: BASE_TEXT_OPTS_L }, BASE_TEXT_OPTS_R]], BASE_TABLE_OPTS);
+	slide.addTable([[{ text: "Image Examples: Image Types", options: BASE_TEXT_OPTS_L }, BASE_TEXT_OPTS_R]], BASE_TABLE_OPTS);
 	slide.addNotes("API Docs: https://gitbrent.github.io/PptxGenJS/docs/api-images.html");
 	slide.slideNumber = { x: "50%", y: "95%", color: COLOR_BLUE };
 
@@ -178,8 +178,6 @@ function genSlide01(pptx) {
 		slide.addImage({ data: SVG_BASE64, x: 10.61, y: 4.77, w: 2.0, h: 2.0, transparency: 50 }); // TEST: `data`
 	}
 
-	/// TODO: , transparency: 69
-
 	// TEST: Ensure framework corrects for missing all header
 	// (Please **DO NOT** pass base64 data without the header! This is a JUNK TEST!)
 	//slide.addImage({ x:5.2, y:2.6, w:0.8, h:0.8, data:'iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAMAAABEpIrGAAAAA3NCSVQICAjb4U/gAAAACXBIWXMAAAjcAAAI3AGf6F88AAAAGXRFWHRTb2Z0d2FyZQB3d3cuaW5rc2NhcGUub3Jnm+48GgAAANVQTFRF////JLaSIJ+AIKqKKa2FKLCIJq+IJa6HJa6JJa6IJa6IJa2IJa6IJa6IJa6IJa6IJa6IJa6IJq6IKK+JKK+KKrCLLrGNL7KOMrOPNrSRN7WSPLeVQrmYRLmZSrycTr2eUb6gUb+gWsKlY8Wqbsmwb8mwdcy0d8y1e863g9G7hdK8htK9i9TAjNTAjtXBktfEntvKoNzLquDRruHTtePWt+TYv+fcx+rhyOvh0e7m1e/o2fHq4PTu5PXx5vbx7Pj18fr49fv59/z7+Pz7+f38/P79/f7+dNHCUgAAABF0Uk5TAAcIGBktSYSXmMHI2uPy8/XVqDFbAAABB0lEQVQ4y42T13qDMAyFZUKMbebp3mmbrnTvlY60TXn/R+oFGAyYzz1Xx/wylmWJqBLjUkVpGinJGXXliwSVEuG3sBdkaCgLPJMPQnQUDmo+jGFRPKz2WzkQl//wQvQoLPII0KuAiMjP+gMyn4iEFU1eAQCCiCU2fpCfFBVjxG18f35VOk7Swndmt9pKUl2++fG4qL2iqMPXpi8r1SKitDDne/rT8vPbRh2d6oC7n6PCLNx/bsEM0Edc5DdLAHD9tWueF9VJjmdP68DZ77iRkDKuuT19Hx3mx82MpVmo1Yfv+WXrSrxZ6slpiyes77FKif88t7Nh3C3nbFp327sHxz167uHtH/8/eds7gGsUQbkAAAAASUVORK5CYII=' });
@@ -188,10 +186,56 @@ function genSlide01(pptx) {
 }
 
 /**
- * SLIDE 2: Image Sizing
+ * SLIDE 4: Image URLs
  * @param {PptxGenJS} pptx
  */
 function genSlide02(pptx) {
+	let slide = pptx.addSlide({ sectionTitle: "Images" });
+
+	slide.addTable([[{ text: "Image Examples: Image URLs", options: BASE_TEXT_OPTS_L }, BASE_TEXT_OPTS_R]], BASE_TABLE_OPTS);
+	slide.addNotes("API Docs: https://gitbrent.github.io/PptxGenJS/docs/api-images.html");
+	slide.slideNumber = { x: "50%", y: "95%", color: COLOR_BLUE };
+
+	// TOP-LEFT: jpg
+	slide.addText(
+		[{ text: `path:"${IMAGE_PATHS.ccLogo.path}"` }],
+		{ ...BASE_CODE_OPTS, ...{ x: 0.5, y: 0.6, w: 3.6, h: 3.35, fontSize: 10, color:'696969', valign: 'bottom', margin: 10, fill: { color: BKGD_LTGRAY }, } }
+	);
+	slide.addImage({ path: IMAGE_PATHS.ccLogo.path, x: 0.66, y: 0.75, h: 2.5, w: 3.33 });
+
+	// TODO: WIP: do same below
+
+	// TOP-CENTER: png
+	slide.addText([{ text: `path:"${IMAGE_PATHS.wikimedia2.path}"` }], { ...BASE_CODE_OPTS, ...{ x: 4.55, y: 0.6, w: 3.27, h: 0.7, fontSize: 10 } });
+	slide.addImage({ path: IMAGE_PATHS.wikimedia2.path, x: 4.55, y: 1.44, h: 2.5, w: 3.27 });
+
+	// TOP-RIGHT: relative-path test
+	slide.addText([{ text: `path:"${IMAGE_PATHS.ccLicenseComp.path}"` }], {
+		...BASE_CODE_OPTS,
+		...{ x: 8.55, y: 0.6, w: 4.28, h: 0.7, fontSize: 11 },
+	});
+	// NOTE: Node will throw exception when using "/" path
+	slide.addImage({ path: `${typeof window === "undefined" ? ".." : ""}${IMAGE_PATHS.ccLicenseComp.path}`, x: 8.55, y: 1.43, h: 2.51, w: 4.28 });
+
+	// BOTTOM: wide, url-sourced
+	slide.addText(
+		[
+			{ text: '// Test: URL variables, plus more than one ".jpg"', options: { breakLine: true } },
+			{ text: `path:"${IMAGE_PATHS.sydneyBridge.path}"` },
+		],
+		{
+			...BASE_CODE_OPTS,
+			...{ x: 0.5, y: 4.2, w: 12.33, h: 0.8, fontSize: 11 },
+		}
+	);
+	slide.addImage({ path: IMAGE_PATHS.sydneyBridge.path, x: 0.5, y: 5.16, h: 1.8, w: 12.33 });
+}
+
+/**
+ * SLIDE 2: Image Sizing
+ * @param {PptxGenJS} pptx
+ */
+function genSlide03(pptx) {
 	let slide = pptx.addSlide({ sectionTitle: "Images" });
 
 	slide.addTable([[{ text: "Image Examples: Image Sizing/Rounding", options: BASE_TEXT_OPTS_L }, BASE_TEXT_OPTS_R]], BASE_TABLE_OPTS);
@@ -226,7 +270,7 @@ function genSlide02(pptx) {
  * SLIDE 3: Image Rotation
  * @param {PptxGenJS} pptx
  */
-function genSlide03(pptx) {
+function genSlide04(pptx) {
 	let slide = pptx.addSlide({ sectionTitle: "Images" });
 
 	slide.addTable([[{ text: "Image Examples: Image Rotation", options: BASE_TEXT_OPTS_L }, BASE_TEXT_OPTS_R]], BASE_TABLE_OPTS);
@@ -238,47 +282,6 @@ function genSlide03(pptx) {
 	slide.addImage({ path: IMAGE_PATHS.tokyoSubway.path, x: 0.78, y: 2.46, w: 4.3, h: 3, rotate: 45 });
 	slide.addImage({ path: IMAGE_PATHS.tokyoSubway.path, x: 4.52, y: 2.25, w: 4.3, h: 3, rotate: 180 });
 	slide.addImage({ path: IMAGE_PATHS.tokyoSubway.path, x: 8.25, y: 2.84, w: 4.3, h: 3, rotate: 315 });
-}
-
-/**
- * SLIDE 4: Image URLs
- * @param {PptxGenJS} pptx
- */
-function genSlide04(pptx) {
-	let slide = pptx.addSlide({ sectionTitle: "Images" });
-
-	slide.addTable([[{ text: "Image Examples: Image URLs", options: BASE_TEXT_OPTS_L }, BASE_TEXT_OPTS_R]], BASE_TABLE_OPTS);
-	slide.addNotes("API Docs: https://gitbrent.github.io/PptxGenJS/docs/api-images.html");
-	slide.slideNumber = { x: "50%", y: "95%", color: COLOR_BLUE };
-
-	// TOP-LEFT: jpg
-	slide.addText([{ text: `path:"${IMAGE_PATHS.ccLogo.path}"` }], { ...BASE_CODE_OPTS, ...{ x: 0.5, y: 0.6, w: 3.3, h: 0.7, fontSize: 11 } });
-	slide.addImage({ path: IMAGE_PATHS.ccLogo.path, x: 0.5, y: 1.44, h: 2.5, w: 3.33 });
-
-	// TOP-CENTER: png
-	slide.addText([{ text: `path:"${IMAGE_PATHS.wikimedia2.path}"` }], { ...BASE_CODE_OPTS, ...{ x: 4.55, y: 0.6, w: 3.27, h: 0.7, fontSize: 11 } });
-	slide.addImage({ path: IMAGE_PATHS.wikimedia2.path, x: 4.55, y: 1.44, h: 2.5, w: 3.27 });
-
-	// TOP-RIGHT: relative-path test
-	slide.addText([{ text: `path:"${IMAGE_PATHS.ccLicenseComp.path}"` }], {
-		...BASE_CODE_OPTS,
-		...{ x: 8.55, y: 0.6, w: 4.28, h: 0.7, fontSize: 11 },
-	});
-	// NOTE: Node will throw exception when using "/" path
-	slide.addImage({ path: `${typeof window === "undefined" ? ".." : ""}${IMAGE_PATHS.ccLicenseComp.path}`, x: 8.55, y: 1.43, h: 2.51, w: 4.28 });
-
-	// BOTTOM: wide, url-sourced
-	slide.addText(
-		[
-			{ text: '// Test: URL variables, plus more than one ".jpg"', options: { breakLine: true } },
-			{ text: `path:"${IMAGE_PATHS.sydneyBridge.path}"` },
-		],
-		{
-			...BASE_CODE_OPTS,
-			...{ x: 0.5, y: 4.2, w: 12.33, h: 0.8, fontSize: 11 },
-		}
-	);
-	slide.addImage({ path: IMAGE_PATHS.sydneyBridge.path, x: 0.5, y: 5.16, h: 1.8, w: 12.33 });
 }
 
 /**
