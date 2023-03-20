5c675424113b8a41e34c20f06d329b5c641589bb
Brent Ely <gitbrent@users.noreply.github.com>
2023-03-19 19:29:01 -0500

    added types

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,7 +20,7 @@ module.exports = {
 		project: ["./tsconfig.json"],
 	},
 	plugins: ["react", "@typescript-eslint"],
-	ignorePatterns: [".eslintrc.js", "*.mjs", "demos/*"],
+	ignorePatterns: [".eslintrc.js", "*.mjs", "demos/*", "index.d.ts"],
 	rules: {
 		"@typescript-eslint/indent": ["error", "tab"],
 		"@typescript-eslint/prefer-nullish-coalescing": 0, // "warn", too many items!
