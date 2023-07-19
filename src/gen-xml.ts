61fb38b9b764775581fadaf889454a113c30a9f4
Ryan Yocum <ryan@rtyocum.dev>
2023-07-19 09:56:01 -0400

    Fixed slide num issue

--- src/gen-xml.ts
+++ src/gen-xml.ts
@@ -1618,7 +1618,7 @@ export function makeXmlMaster (slide: PresSlide, layouts: SlideLayout[]): string
 	strXml +=
 		'<p:clrMap bg1="lt1" tx1="dk1" bg2="lt2" tx2="dk2" accent1="accent1" accent2="accent2" accent3="accent3" accent4="accent4" accent5="accent5" accent6="accent6" hlink="hlink" folHlink="folHlink"/>'
 	strXml += '<p:sldLayoutIdLst>' + layoutDefs.join('') + '</p:sldLayoutIdLst>'
-	strXml += '<p:hf sldNum="0" hdr="0" ftr="0" dt="0"/>'
+	strXml += slide._slideNumberProps !== null ? '<p:hf hdr="0" ftr="0" dt="0"/>' : '<p:hf sldNum="0" hdr="0" ftr="0" dt="0"/>'
 	strXml +=
 		'<p:txStyles>' +
 		' <p:titleStyle>' +
